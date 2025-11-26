6396e054e7276e70a819eaaf42bb9ae71b2802b8
Arthur Franco <153031237+Ima23-d@users.noreply.github.com>
2025-11-26 01:07:19 -0300

    Refactor SQL queries for student training view

--- DQL/DQL.sql
+++ DQL/DQL.sql
@@ -1,15 +1,25 @@
-SELECT Nome_Aluno, Idade, Nivel FROM Alunos;
+-- -------------------
+-- Ver Treino do Aluno
+-- -------------------
+SELECT treinos.id_treinos, treinos.especificacoes, instrutores.nome
+        FROM treinos
+        JOIN instrutores ON treinos.id_instrutor = instrutores.id_instrutor
+        JOIN treinos_alunos ON treinos.id_treinos = treinos_alunos.id_treinos
+        WHERE treinos_alunos.id_aluno = %s
+  
+-- -- --------------------
+-- Cadastrar Treino(buscar)
+-- ------------------------
+SELECT id_aluno FROM alunos WHERE nome_aluno ILIKE %s
 
-SELECT A.Nome_Aluno, P.Nome_Plano, P.Valor
-FROM Alunos A
-JOIN Escolhe E ON A.ID_Aluno = E.ID_Aluno
-JOIN Planos P ON E.ID_Planos = P.ID_Planos
-WHERE A.ID_Aluno = 1;
-
-
-SELECT A.Nome_Aluno, T.Especificacoes, I.Nome AS Instrutor
-FROM Alunos A
-JOIN Realiza R ON A.ID_Aluno = R.ID_Aluno
-JOIN Treinos T ON R.ID_Treinos = T.ID_Treinos
-JOIN Instrutores I ON T.ID_Instrutor = I.ID_Instrutor
-WHERE A.ID_Aluno = 1;
+-- -- ----------------------
+-- Ver Treino Aluno Personal
+-- -------------------------
+ SELECT 
+        treinos.id_treinos, 
+        treinos.especificacoes, 
+        instrutores.nome AS nome_instrutor
+    FROM treinos
+    JOIN instrutores ON treinos.id_instrutor = instrutores.id_instrutor
+    JOIN treinos_alunos ON treinos.id_treinos = treinos_alunos.id_treinos
+    WHERE treinos_alunos.id_aluno = %s
